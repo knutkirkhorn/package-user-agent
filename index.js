46a0503f35cf70099da837a159e002de46f0869d
Knut Kirkhorn <knutkirk@hotmail.com>
2023-02-17 13:08:51 +0100

    Return the User-Agent directly

--- index.js
+++ index.js
@@ -10,8 +10,7 @@ export default async function getPackageUserAgent() {
 	const {name, repository} = await readPackage();
 	const url = repository && repository.url ? parsePackageRepositoryUrl(repository.url) : undefined;
 	const urlPart = url ? ` (${url})` : '';
-	const userAgent = {
+	return {
 		'User-Agent': `${name}${urlPart}`
 	};
-	return userAgent;
 }
