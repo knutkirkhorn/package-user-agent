d2de0f91b10aa3ceacb2a9bb649b58eadbebfe64
Knut Kirkhorn <knutkirk@hotmail.com>
2023-02-25 10:26:07 +0100

    Tweak type definition test

--- index.test-d.ts
+++ index.test-d.ts
@@ -1,4 +1,4 @@
 import {expectType} from 'tsd';
-import getPackageUserAgent, {UserAgentHeader} from ".";
+import getPackageUserAgent, {UserAgentHeader} from './index.js';
 
 expectType<Promise<UserAgentHeader>>(getPackageUserAgent());
